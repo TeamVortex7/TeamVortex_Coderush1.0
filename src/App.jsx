c40f32a5fb1ecaf48f38b8a80c0e4ccc844e2899
TeamVortex <vortex.official77@gmail.com>
2025-08-22 23:37:21 +0530

    Added Dashboard Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
-import React from "react";
-import LandingPage from "./pages/LandingPage";
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// Import all the pages of your application
+import LandingPage from './pages/LandingPage';
+
+import DashboardPage from './pages/DashboardPage';
 
 function App() {
-  return <LandingPage />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* Route for the homepage */}
+        <Route path="/" element={<LandingPage />} />
+
+        {/* Routes for authentication */}
+        {/* Route for the user dashboard (after login) */}
+        <Route path="/dashboard" element={<DashboardPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
