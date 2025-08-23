a4b6f4858a5a91d4cd93978af81278921feb0610
TeamVortex <vortex.official77@gmail.com>
2025-08-23 06:45:52 +0530

    Deployment 1

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,22 +1,17 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// The config object now reads from Vercel's environment variables
 const firebaseConfig = {
-  apiKey: "AIzaSyA7EO0mu4qHCoXMQpL6ofXGn7-do9pawGU",
-  authDomain: "teamvortex-auth-project.firebaseapp.com",
-  projectId: "teamvortex-auth-project",
-  storageBucket: "teamvortex-auth-project.firebasestorage.app",
-  messagingSenderId: "105311199304",
-  appId: "1:105311199304:web:0921f1a28bd1f38c4f787d",
-  measurementId: "G-D44XLY79PW"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
